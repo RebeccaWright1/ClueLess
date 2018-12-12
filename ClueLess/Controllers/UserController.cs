da82a99ef2eca3c6fe6678745e5e606aa1851cbc
Xian Li <xli205@jh.edu>
2018-12-11 18:30:00 -0800

    should work

--- ClueLess/Controllers/UserController.cs
+++ ClueLess/Controllers/UserController.cs
@@ -10,9 +10,10 @@ using System.Web.Http;
 
 namespace ClueLess.Controllers
 {
-    [RoutePrefix("api/User")]
     public class UserController : ApiController
     {
+        [HttpPost, HttpGet]
+        [Route("UserController/CreateAccount/")]
         public IHttpActionResult CreateAccount(Account userAccount)
         {
             try
@@ -21,54 +22,68 @@ namespace ClueLess.Controllers
                 return Ok();
 
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 return Content(HttpStatusCode.BadRequest, "Unable to save account");
             }
-            
+
         }
 
-        public IHttpActionResult SignIn(String username, String password)
+        [HttpPost]
+        [Route("UserController/SignIn/")]
+        public IHttpActionResult SignIn(string username, string password)
         {
             //Add code that starts the users session if validated
             int id = Account.ValidateUser(username, password);
-            if(id>0)
+            if (id > 0)
             {
-              
-                HttpContext.Current.Session["userID"] = id;
+                //HttpContext.Current.Session["userID"] = id;
+                return Ok("success");
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [Route("UserController/SignOut/")]
+        public IHttpActionResult SignOut()
+        {
+            //Add code that ends users session
+            //HttpContext.Current.Session.Abandon();
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("UserController/ResetPassword/")]
+        public IHttpActionResult ResetPassword(string username, string oldPassword, string newPassword)
+        {
+            int userID = Account.ValidateUser(username, oldPassword);
+            if (userID > 0)
+            {
+                Account.ResetPassword(userID, newPassword);
                 return Ok();
             }
             else
             {
                 return BadRequest();
             }
-            
+
         }
 
-        public IHttpActionResult SignOut()
-        {
-            //Add code that ends users session
-            HttpContext.Current.Session.Abandon();
-            return Ok();
-        }
-
-        public IHttpActionResult ResetPassword(int userID, string newPassword)
-        {
-            Account.ResetPassword(userID, newPassword);
-            return Ok();
-        }
-
+        [HttpPost]
         public IHttpActionResult ForgotUsername(string emailAddresss)
         {
             Account.RequestUserName(emailAddresss);
             return Ok();
         }
 
-
-        public Account EditAccount(int userID)
-        {
-            Account newAccount = new Account();
-            return newAccount;
-        }
+        //[HttpPost]
+        //public Account EditAccount(int userID)
+        //{
+        //    Account newAccount = new Account();
+        //    return newAccount;
+        //}
     }
 }
